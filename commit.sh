6dedfd53c63d4910ba8926cd4b8de5a7098b4097
Levi <levirankin1@gmail.com>
2025-10-28 14:22:29 -0700

    Auto-commit

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Auto-commit script
-
-# Get current branch name
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-git add .                              # Stage all changes
-git commit -m "Auto-commit"           # Commit with fixed message
-git push origin "$BRANCH" --force      # Push to current branch forcefully
